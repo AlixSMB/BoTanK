7bfcbb4fbde0b2f34df3c909dc56a3ebedf379a1
AlixSMB <90306694+AlixSMB@users.noreply.github.com>
2022-12-03 11:00:24 +0100

    Add files via upload

---
+++ SOFTWARE/tank/tank_software.py
@@ -0,0 +1,151 @@
+from util import * # util.py
+
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+
+import pickle
+
+#CAMERADATA_FILENAME = "camera_calibration_params_1"
+#print(f"Reading camera calibration params from \"{CAMERADATA_FILENAME}\"")
+#with open(CAMERADATA_FILENAME, "rb") as filecamera : cameradata = pickle.load(filecamera)
+
+
+class CtrlPanelServer:
+	def __init__(self, port, dietimeout, on_msg, on_close=noop, data=None):
+		self.serv = HTTPServer(port, partial(on_close, self))
+		self.dietimeout = dietimeout
+		self.data = data
+		self.on_msg = on_msg
+	
+	def checkdead(self):
+		for addr in list(self.serv.con.remotes): # list(...) because we can't modify the dict size and iterate it at the same time
+			if self.serv.con.remotes[addr].nodata_elapsed() >= self.dietimeout:
+				self.serv.close(addr)
+
+timers = {}
+def set_timer(name, dt):
+	timers[name] = [0, dt, False]
+def check_timer(name):
+	return timers[name][2]
+def timers_start():
+	newt = time.perf_counter()
+	for timer in timers:
+		if newt-timers[timer][0] >= timers[timer][1]:
+			timers[timer][2] = True
+			timers[timer][0] = newt
+def timers_end():
+	for timer in timers : timers[timer][2] = False
+
+# receive camera feed
+#httpvideo = HTTPVideoStream('10.7.177.245', '8080', '/video/mjpeg')
+vid = cv2.VideoCapture(0)
+
+print(f"Local IP addresses: {getLocalIps()}")
+
+def httpserv_sendvec2d(serv, addr, vec):
+	serv.sendraw(http_txtresp(
+		200, 'OK',
+		bytes(f'{str(vec[0])};{str(vec[1])}', 'utf-8')
+	), addr)
+def httpserv_recvvec2d(serv, addr):
+	try:
+		x, y = parser.body.decode('utf-8').split(';')
+	except:
+		serv.sendraw(bytes('HTTP/1.1 400 BAD VALUES\r\n\r\n', 'utf-8'), addr)
+		return None
+	else:
+		serv.sendraw(bytes('HTTP/1.1 200 OK\r\n\r\n', 'utf-8'), addr)
+		return float(x), float(y)
+
+def localcam_onmsg(self, addr):
+	if self.serv.readfull(addr): # we parsed the full request
+		parser = self.serv.parsers[addr]
+		if parser.url == b'/video/mjpeg' and parser.method == b'GET':
+			sent = self.serv.sendraw(bytes(
+				'HTTP/1.1 200 OK\r\n' +
+				'Cache-Control: no-cache\r\n' +
+				'Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n'
+			, 'utf-8'), addr)
+			if sent is True:
+				self.data['streams'].add(addr)
+				set_timer('localcamfps', 1/20)
+		else:
+			self.serv.sendraw(bytes(
+				'HTTP/1.1 400 BAD REQUEST\r\n\r\n'
+			, 'utf-8'), addr)
+	
+	if addr in self.data['streams'] and check_timer('localcamfps'):
+		self.serv.sendraw(
+			bytes(
+				"--frame\r\n" +
+				"Content-Type: image/jpeg\r\n" +
+				"Content-Length: " + str(len(camera_jpegbytes)) + "\r\n\r\n",
+			'utf-8') +
+			camera_jpegbytes +
+			bytes("\r\n",'utf-8')
+		, addr)
+def coms_onmsg(self, addr):
+	if self.serv.readfull(addr): # we parsed the full request
+		parser = self.serv.parsers[addr]
+		
+		if parser.method == b'GET':
+			if parser.url == b'/pos'    : httpserv_sendvec2d(self.serv, addr, tankpos)
+			elif parser.url == b'/speed': httpserv_sendvec2d(self.serv, addr, tankrealspeed)
+		elif parser.method == b'PUT':
+			if parser.url == b'/auto/on':
+				pass
+				#...
+			elif parser.url == b'/auto/off':
+				pass
+				#...
+			elif parser.url == b'/speed':
+				httpserv_recvvec2d(self.serv, addr)
+				#...
+			elif parser.url == b'/targetpos':
+				httpserv_recvvec2d(self.serv, addr)
+				#...
+ctrlpanel = { 
+	# local cam mjpeg streaming server
+	'localcamera': CtrlPanelServer(
+		8080, 3, localcam_onmsg,
+		on_close=lambda panel, addr: panel.data['streams'].discard(addr), 
+		data={'streams': set()}
+	),
+	'coms': CtrlPanelServer(
+		8081, 10, coms_onmsg
+	)
+}
+
+set_timer('sockalive', 0.5) # might be unnecessary, but may help performance
+set_timer('sockconnect', 0.1)
+while True:
+	global camera_frame, camera_jpegbytes, tankpos, tank_realspeed, tank_targetspeed
+	timers_start()
+	
+	#camera_jpegbytes = httpvideo.getFrameImg()
+	#camera_frame = cv2.imdecode( np.frombuffer(camera_jpegbytes, dtype=np.uint8), cv2.IMREAD_COLOR)
+	_, camera_frame = vid.read()
+	camera_jpegbytes = cv2.imencode('.jpeg', camera_frame)[1].tobytes()
+	#cv2.imshow('videostream', camera_frame)
+	
+	# compute tank pos, speed
+	
+	
+	# listen for http connections
+	if check_timer('sockconnect'):
+		for part in ctrlpanel : ctrlpanel[part].serv.connect()
+	# check connections
+	if check_timer('sockalive'):
+		for part in ctrlpanel : ctrlpanel[part].checkdead()
+	# handle received messages
+	for part in ctrlpanel:
+		for addr in list(ctrlpanel[part].serv.con.remotes) : ctrlpanel[part].on_msg( ctrlpanel[part], addr )
+	
+	if cv2.waitKey(1) == ord('q') : break
+	timers_end()
+
+
+# TODO:
+# 	- optimize code:
+#		- for ... in list(...dict...)
---
+++ SOFTWARE/tank/util.py
@@ -0,0 +1,142 @@
+import socket
+import httptools
+import time
+
+
+def partial(fun, *args1):
+	return lambda *args2: fun(*args1, *args2)
+def noop():
+	pass
+
+# create anonymous objects
+Object = lambda **kwargs: type("Object", (), kwargs)()
+
+def getLocalIps():
+	return socket.gethostbyname_ex(socket.gethostname())[-1]
+
+class HTTPVideoStream:	
+	def __init__(self, addr, port, path):		
+		self.data = bytes()
+		print(f"Connecting to \"{addr}:{port}{path}\" video stream...")
+		self.sock = socket.create_connection((addr, port))
+		self.sock.sendall(bytes( 'GET /video/mjpeg HTTP/1.1\r\n\r\n', 'utf-8' ))
+		print(f"Connected to \"{addr}:{port}{path}\" video stream")
+
+	def getFrameImg(self):
+		while True:
+			self.data += self.sock.recv(1024)	
+			# we should use the boundary marker instead but whatever...		
+			jpegstart = self.data.find( b'\xff\xd8' ) # start of jpeg image data
+			jpegend = self.data.find( b'\xff\xd9' ) # end of jpeg image data
+			if jpegstart != -1 and jpegend != -1:
+				frame = self.data[jpegstart : jpegend+2]
+				self.data = self.data[jpegend+2 :]
+				return frame
+
+class SockInfo:
+	def __init__(self, sock, addr):
+		self.sock = sock
+		self.addr = addr
+		self.lastexch = time.perf_counter() # last time data was transmitted
+	def nodata_elapsed(self):
+		return time.perf_counter() - self.lastexch
+class TCPServer:
+	def __init__(self, port):
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.bind(('', port))
+		self.sock.listen() # only connect with control panel
+		self.sock.setblocking(0)
+		self.remotes = {}
+		
+	def connect(self):
+		try: 
+			remotesock, addr = self.sock.accept()
+			remotesock.setblocking(0)
+			print(f"Remote connection from {addr[0]}:{addr[1]}")
+			self.remotes[addr] = SockInfo(remotesock, addr)
+			return addr
+		except:
+			return None
+	def disconnect(self, addr):
+		print(f"Remote connection from {addr[0]}:{addr[1]} ended")
+		self.remotes[addr].sock.close()
+		del self.remotes[addr]
+	
+	def recv(self, addr):
+		try:
+			remote = self.remotes[addr]
+			data = remote.sock.recv(4096)
+			remote.lastexch = time.perf_counter()
+			return data
+		except:
+			return None
+	def send(self, data, addr):
+		try:
+			remote = self.remotes[addr]
+			remote.sock.sendall(data)
+			remote.lastexch = time.perf_counter()
+			return True
+		except socket.error: # socket disconnected
+			return None
+		return False
+class HttpParserHandler:
+	def __init__(self):
+		self.parser = httptools.HttpRequestParser(self)
+		self.reset()
+	
+	def reset(self):
+		self.complete = False
+		self.headers = {}
+		self.url = None
+		self.method = None
+		self.body = None
+	
+	# httptools callbacks
+	def on_url(self, url):
+		self.url = url
+	def on_message_complete(self):
+		self.complete = True
+		self.method = self.parser.get_method()
+	def on_header(self, name, value):
+		self.headers[name] = value 
+	def on_body(self, body):
+		self.body = body
+class HTTPServer:
+	def __init__(self, port, on_close=noop):
+		self.con = TCPServer(port)
+		self.parsers = {}
+		
+		self.on_close = on_close
+	
+	def connect(self):
+		addr = self.con.connect()
+		if addr is not None : self.parsers[addr] = HttpParserHandler()
+	def close(self, addr):
+		del self.parsers[addr]
+		self.con.disconnect(addr)
+		self.on_close(addr)
+	
+	def read(self, addr):
+		data = self.con.recv(addr)
+		if data is not None:
+			self.parsers[addr].parser.feed_data(data)
+			return True
+		else:
+			return False
+	def readfull(self, addr):
+		return self.read(addr) and self.parsers[addr].complete
+	
+	def sendraw(self, data, addr):
+		sent = self.con.send(data, addr)
+		if sent is None: # disconnect
+			self.close(addr)
+			return None
+		else : return sent
+
+
+def http_txtresp(code, status, bindata):
+	return bytes(
+		f'HTTP/1.1 {code} {status}\r\n' +
+		'Content-Type: text/plain; charset=utf-8\r\n' +
+		f'Content-Length: {len(bindata)}\r\n\r\n'
+	, 'utf-8') + bindata
