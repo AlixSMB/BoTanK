9930191e1d13b4eee9da03337b533bd41737cfa8
AlixSMB <90306694+AlixSMB@users.noreply.github.com>
2022-12-07 10:18:57 +0100

    Add files via upload

--- SOFTWARE/control_panel/index.js
+++ SOFTWARE/control_panel/index.js
@@ -6,7 +6,8 @@ let uniqueid = (function(){
 	return function(){ return ++tmp; }
 })();
 
-function resetNodesVals(nodes){ nodes.forEach(node => node.value = node.getAttribute('oldvalue')); }
+function resetNodesVals(nodes) { nodes.forEach(node => node.value = node.getAttribute('oldvalue')); }
+function updateNodesVals(nodes){ nodes.forEach(node => node.setAttribute('oldvalue', node.value)); }
 let html_inputzone = (on_ok, n=1, values=null, attrs=null, sep="<b>;</b>") => {
 	let inputs = [...Array(n).keys()].map(ind => 
 		`<input 
@@ -23,7 +24,6 @@ let html_inputzone = (on_ok, n=1, values=null, attrs=null, sep="<b>;</b>") => {
 				<input type="image" src="res/check.png" class="btn_ok" onclick="
 					let nodes = getdom('input', this.parentNode.parentNode).splice(0, ${n});
 					${on_ok}(nodes);
-					nodes.forEach(node => node.setAttribute('oldvalue', node.value));
 					this.parentNode.style.display = 'none';
 				"></input>
 				<input type="image" src="res/cancel.png" class="btn_cancel" onclick="
@@ -37,39 +37,51 @@ let html_inputzone = (on_ok, n=1, values=null, attrs=null, sep="<b>;</b>") => {
 
 let msgconsole = getdom('#div_msg')[0];
 
+let canvas = getdom('canvas')[0];
+let canvasW = canvas.width; let canvasH = canvas.height;
+let ctx = canvas.getContext('2d');
+ctx.scale(1, -1);           // set y axis pointing up
+ctx.translate(0, -canvasH); // origin at bottom left
+
 let tanks = [];
 class Tank{
-	constructor( addr="localhost" ){
+	static colors = ['red', 'green', 'blue', 'orange', 'purple'];
+	
+	constructor( addr="localhost", color=null ){
 		this.move = {
 			com: { // command
-				pos: { x: 0, y: 0 },
-				dir: { x: 0, y: 0 },
-				vel: { x: 0, y: 0 }
+				pos: [0, 0], // x, y
+				dir: [0, 0],
+				vel: [0, 0]
 			},
 			real: { // actual value
-				pos: { x: 0, y: 0 },
-				dir: { x: 0, y: 0 },
-				vel: { x: 0, y: 0 }
+				pos: [0, 0], 
+				dir: [0, 0],
+				vel: [0, 0]
 			},
 			auto: { // automatic mode
 				on: false,
-				target: {x: 0, y: 0}
+				target: [0, 0]
 			}
 		};
 		this.cannon = {
-			com: {
-				yaw: 0,
-				pitch: 0
-			},
-			real: {
-				yaw: 0,
-				pitch: 0
-			},
-			auto: {on: false}
+			com:  [0, 0], // yaw, pitch
+			real: [0, 0],
+			auto: { on: false }
 		};
 		
 		this.id = uniqueid();
 		this.addr = addr;
+		this.netloops = 0;
+		this.netstop = false;
+		
+		this.color = color === null ? Tank.colors[ Tank.colors.length % this.id ] : color;
+		this.path = new Path2D();
+		let base = 10; let height = 20;
+		this.path.moveTo(-base/2,0);
+		this.path.lineTo(base/2, 0);
+		this.path.lineTo(0, height);
+		this.path.closePath();
 	}
 	
 	setAddr(addr){
@@ -77,88 +89,173 @@ class Tank{
 		getdom(`img[tankid="${this.id}"]`)[0].src = `http://${this.addr}:8080/video/mjpeg`;
 	}
 	
-	setData(path, data, on_ok, on_errcode, on_errnet){
-		let self = this;
+	toggleNeterror(on){
+		this.netstop = on;
+		getdom(`.span_unreachable[tankid="${this.id}"]`)[0].style.display = on ? 'inline' : 'none';
+	}
+	
+	setData(path, data, on_ok, on_errcode, on_errnet, isloop){
+		if (this.netstop){
+			if (isloop) this.netloops--; 
+			return;
+		}
+		
 		fetch(`http://${this.addr}:8081${path}`, {method: 'PUT', body: data})
 		.then(res => {
-			if (res.ok) on_ok(self);
-			else on_errcode(res.status, res.statusText, self);
+			if (res.ok) on_ok();
+			else{
+				if (isloop) this.netloops--;
+				on_errcode(res.status, res.statusText);
+				this.toggleNeterror(true);
+			}
 		})
-		.catch( err => on_errnet(err, self) );
+		.catch(err => {
+			if (isloop) this.netloops--;
+			on_errnet(err);
+			this.toggleNeterror(true);
+		});
 	}
-	setVec2d(nodes, x, y, path, typestr, obj){
+	setVec2d(x, y, parts, msg=false, callback=null, isloop=false, nodes=null){
+		let path = '/'+parts.join('/');
 		this.setData(
 			path, `${x.toFixed(1)};${y.toFixed(1)}`, 
-			self => {
-				obj.x = x; obj.y = y;
-				self.dispmsg(`${typestr} set to (${x};${y})`);
+			() => {
+				let obj = this[parts[0]][parts[1]][parts[2]];
+				[obj.x, obj.y] = [x, y];
+				if (nodes !== null) updateNodesVals(nodes);
+				if (msg) this.dispmsg(`${path} set to (${x};${y})`);
+				if (callback !== null) callback();
 			}, 
-			(code, status, self) => {
+			(code, status) => {
 				if (nodes !== null) resetNodesVals(nodes);
-				self.dispmsg(`error setting ${typestr} (${code} ${status})`);
+				self.dispmsg(`error setting ${path} (${code} ${status})`);
 			},
-			(err, self) => self.dispmsg(`error setting ${typestr} (${err})`)
+			err => {
+				if (nodes !== null) resetNodesVals(nodes);
+				self.dispmsg(`error setting ${path} (${err})`);
+			},
+			isloop
 		);
 	}
-	setVel(x, y){              this.setVec2d(null,  x, y, '/move/real/vel',    'velocity',    this.move.com.vel    ); }
-	setTargetpos(nodes, x, y){ this.setVec2d(nodes, x, y, '/move/auto/target', 'target pos.', this.move.auto.target); }
-	toggleAuto(node, on, type){
+	setBool(parts, on, msg=false, node=null){
+		let path = '/'+parts.join('/');
 		this.setData(
-			`/${type}/auto/${on ? 'on' : 'off'}`, '', 
-			self => {
-				self[type].auto.on = on;
-				self.dispmsg(`autonomous ${type} ${on ? 'enabled' : 'disabled'}`);
+			path + on ? '/on' : '/off', ''
+			() => {
+				this[parts[0]][parts[1]].on = on;
+				if (msg) self.dispmsg(`${parts} set to ${on ? 'enabled' : 'disabled'}`);
 			}, 
-			(code, status, self) => {
-				node.checked = self[type].auto.on;
-				self.dispmsg(`error ${on ? 'enabling' : 'disabling'} autonomous ${type} (${code} ${status})`);
+			(code, status) => {
+				if (node !== null) node.checked = this[parts[0]][parts[1]].on;
+				self.dispmsg(`error setting ${parts} (${code} ${status})`);
 			},
-			(err, self) => self.dispmsg(`error ${on ? 'enabling' : 'disabling'} autonomous ${type} (${err})`)
+			(err, self) => self.dispmsg(`error setting ${parts} (${err})`)
 		);
 	}
 	
-	getData(path, on_ok, on_errcode, on_errnet){
-		let self = this;
+	getData(path, on_ok, on_errcode, on_errnet, isloop){
+		if (this.netstop){
+			if (isloop) this.netloops--; 
+			return;
+		}
+		
 		fetch(`http://${this.addr}:8081${path}`, {method: 'GET'})
 		.then(res => {
-			if (res.ok) res.text().then(txt => on_ok(txt, self)).catch(err => on_errnet(err, self));
-			else on_errcode(res.status, res.statusText, self);
+			if (res.ok) res.text().then(txt => on_ok(txt)).catch(err => {
+				if (isloop) this.netloops--;
+				on_errnet(err); 
+				this.toggleNeterror(true);
+			});
+			else{
+				if (isloop) this.netloops--;
+				on_errcode(res.status, res.statusText);
+				this.toggleNeterror(true);
+			}
 		})
-		.catch( err => on_errnet(err, self) );
+		.catch(err => {
+			if (isloop) this.netloops--;
+			on_errnet(err);
+			this.toggleNeterror(true);
+		});
 	}
-	getVec2d(path, typestr, obj){
+	getVec2d(parts, msg=false, callback=null, isloop=false){
+		let path = '/'+parts.join('/');
 		this.getData(
 			path, 
-			(txt, self) => {
+			txt => {
 				try{
 					let [x,y] = txt.split(";");
-					obj.x = Number(x); obj.y = Number(y);
-					self.dispmsg(`${typestr} is (${x};${y})`);
+					let obj = this[parts[0]][parts[1]][parts[2]];
+					[obj.x, obj.y] = [Number(x), Number(y)]; 
+					if (msg) this.dispmsg(`${path} is (${x};${y})`); 
+					if (callback !== null) callback();
 				}
-				catch (err){ self.dispmsg(`error getting ${typestr} (${err})`); }
+				catch (err){ this.dispmsg(`error getting ${path} (${err})`); }
 			}, 
-			(code, status, self) => self.dispmsg(`error getting ${typestr} (${code} ${status})`),
-			(err, self) => self.dispmsg(`error getting ${typestr} (${err})`)
+			(code, status) => this.dispmsg(`error getting ${path} (${code} ${status})`),
+			err            => this.dispmsg(`error getting ${path} (${err})`),
+			isloop
 		);
 	}
-	getPos(){ this.getVec2d("/move/real/pos", "real pos.", this.move.real.pos); }
-	getDir(){ this.getVec2d("/move/real/dir", "real dir.", this.move.real.dir); }
-	getAuto(type){
+	getBool(parts, msg=false){
+		let path = '/'+parts.join('/');
 		this.getData(
-			`/${type}/auto/on`, 
-			(txt, self) => {
-				if (txt == '1')     { self.dispmsg(`autonomous ${type} is enabled`); self[type].auto.on = true;   }
-				else if (txt == '0'){ self.dispmsg(`autonomous ${type} is disabled`); self[type].auto.on = false; }
-				else                  self.dispmsg(`error getting autonomous ${type} status`);
+			path+'/on', 
+			txt => {
+				if (txt == '1'){ 
+					this[parts[0]][parts[1]].on = true;   
+					if (msg) this.dispmsg(`${path} is on`); 
+				}
+				else if (txt == '0'){ 
+					this[parts[0]][parts[1]].on = false;   
+					if (msg) this.dispmsg(`${path} is off`); 
+				}
+				else this.dispmsg(`error getting ${path}`);
 			}, 
-			(code, status, self) => self.dispmsg(`error getting autonomous ${type} status (${code} ${status})`),
-			(err, self) => self.dispmsg(`error getting autonomous ${type} status (${err})`)
+			(code, status) => this.dispmsg(`error getting ${path} (${code} ${status})`),
+			err            => this.dispmsg(`error getting ${path} (${err})`),
+			false
 		);
+	}
+	
+	// sends new request once the answer to the previous one arrives
+	getPosLoop(){ this.getVec2d(['move', 'real', 'pos'], false, () => this.getPosLoop(), true); }
+	getDirLoop(){ this.getVec2d(['move', 'real', 'dir'], false, () => this.getDirLoop(), true); }
+	
+	getAll(callback){
+		this.netstop = true; // stop the running loops 
+		let id = window.setInterval(() => {
+			if (this.netloops == 0){
+				this.netstop = false;
+				
+				this.getBool( ['move', 'auto'] );
+				this.getBool( ['cannon', 'auto'] );
+				this.netloops++; this.getPosLoop();
+				this.netloops++; this.getDirLoop();
+				
+				window.clearInterval(id);
+				callback();
+			}
+		}, 100);
 	}
 	
 	refresh(){
-		getdom(`.div_tank[tankid="${this.id}"] .btn_ok`).forEach(el => el.click());
-		getdom(`.div_tank[tankid="${this.id}"] .check_com`).forEach(el => el.onchange());
+		this.toggleNeterror(false);
+		
+		// set values
+		//getdom(`.div_tank[tankid="${this.id}"] .btn_ok`).forEach(el => el.click());
+		//getdom(`.div_tank[tankid="${this.id}"] .check_com`).forEach(el => el.onchange());
+		// get values
+		this.getAll( ()=>this.dispmsg('connection refreshed') );
+	}
+	
+	draw(){
+		ctx.save();
+		ctx.fillStyle = this.color;
+		ctx.translate(this.move.real.pos.x, this.move.real.pos.y);
+		ctx.rotate(-Math.tan(this.move.real.dir.y / this.move.real.dir.x));
+		ctx.fill(this.path);
+		ctx.restore();
 	}
 	
 	dispmsg(msg){
@@ -174,7 +271,7 @@ function addTank(){
 	getdom('#div_tanks')[0].innerHTML += `
 		<div class="div_tank" ${tankidattr}>
 			<div>
-				Tank ${html_inputzone("in_tankaddr", 1, [tank.addr], [tankidattr+" size=6"])}
+				<b>Tank</b> ${html_inputzone("in_tankaddr", 1, [tank.addr], [tankidattr+" size=6"])} <span class="span_unreachable" ${tankidattr}>unreachable</span>
 				<input type="image" src="res/sync.png" onclick="tankfromid(${tank.id}).refresh()" class="btn_refresh"></input>
 			</div>
 			<input type="checkbox" onchange="toggle_auto(this, 'move');" checked="${tank.move.auto.on}" class="check_com" ${tankidattr}></input>Auto move<br>
@@ -182,7 +279,7 @@ function addTank(){
 			<div>
 				Target pos.: ${html_inputzone(
 					"in_targetpos", 2, 
-					[tank.move.com.pos.x.toFixed(1), tank.move.com.pos.y.toFixed(1)],
+					[tank.move.com.pos[0].toFixed(1), tank.move.com.pos[0].toFixed(1)],
 					[tankidattr+" size=2", tankidattr+" size=2"]
 				)}
 			</div>
@@ -200,3 +297,17 @@ function in_tankaddr(nodes){ tankfromnode(nodes[0]).setAddr(nodes[0].value); }
 function in_targetpos(nodes){ tankfromnode(nodes[0]).setTargetpos(nodes, Number(nodes[0].value), Number(nodes[1].value)); }
 function toggle_auto(node, type){ tankfromnode(node).toggleAuto(node, node.checked, type); }
 function toggle_camerafeed(node){ getdom('img', node.parentNode)[0].style.display = node.checked ? 'block' : 'none'; }
+
+
+let fps = 25;
+function loop(){
+	ctx.clearRect(0,0, canvasW, canvasH);
+	tanks.forEach( tank => tank.draw() );
+}
+window.setInterval(loop, 1000/fps)
+
+
+/*
+TODO:
+
+*/
